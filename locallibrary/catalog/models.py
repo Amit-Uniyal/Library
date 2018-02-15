da49aeb2f694f45e36495718c66903e515335157
amit <auniyal61@gmail.com>
2018-02-16 00:00:00 +0530

    Added admin feature, reverse-url

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -22,16 +22,16 @@ class Author(models.Model):
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        return '{0},{1}'.format(self.first_name, self.last_name)
+        return '{0} {1}'.format(self.first_name, self.last_name)
 
-class Book():
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13, help_text="13 character")
     genre = models.ManyToManyField(Genre, help_text="Select genre for this book")
 
-    def _get_absolute_url_(self):
+    def get_absolute_url(self):
         '''
          /application/modelname/2
          where 2 is a id in record
@@ -40,8 +40,8 @@ class Book():
         '''
         return reverse('book-detail', args=[str(self.id)])
 
-        def __str__(self):
-            return self.title.capitalize()
+    def __str__(self):
+        return self.title.capitalize()
 
 class BookInstance(models.Model):
     '''
