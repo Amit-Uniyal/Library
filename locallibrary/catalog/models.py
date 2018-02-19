0909b5f9fdef3947b03dd86c5b7d3bd63e91c550
amit <auniyal61@gmail.com>
2018-02-19 23:11:34 +0530

    Added ModelAdmin class, Configure list view

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -40,6 +40,16 @@ class Book(models.Model):
         '''
         return reverse('book-detail', args=[str(self.id)])
 
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all() ])
+        # return self.genre
+    display_genre.short_description = 'Genre'
+
+
     def __str__(self):
         return self.title.capitalize()
 
