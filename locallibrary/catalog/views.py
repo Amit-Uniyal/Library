fc530953cae86ad6b894ff9937c781e7974e0a40
amit <auniyal61@gmail.com>
2018-02-23 14:49:23 +0000

    Added index and base_generic page as templates Added css as static Updated views to render index page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+from .models import Book, Author, Genre, BookInstance
 
-# Create your views here.
+
 
 def index(request):
-	now = datetime.now()
-	html = "<h2>It is now %s.</h2>"  % now.ctime()
-	return HttpResponse(html)
+	'''
+	index page for the catalog app, as well as site
+	'''
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+
+	# Available books (status = 'a')
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+	num_authors = Author.objects.all().count()
+
+	context = {
+		'num_books': num_books, 'num_instances': num_instances,
+		'num_instances_available': num_instances_available,
+		'num_authors': num_authors,
+
+	}
   
+	return render(
+		request,
+		'index.html',
+		context=context 
+	)
